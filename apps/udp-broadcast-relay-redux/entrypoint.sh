f1388e3861a275f9ca7c5e17397c412670ac45fc
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2021-11-16 10:38:23 +0100

    feat: Make separator configurable (#164)
    Signed-off-by: Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>

--- apps/udp-broadcast-relay-redux/entrypoint.sh
+++ apps/udp-broadcast-relay-redux/entrypoint.sh
@@ -4,38 +4,42 @@ set -e
 
 args=()
 
+if [ -z "$SEPARATOR" ]; then
+    SEPARATOR=", "
+fi
+
 if [ -n "$CFG_ID" ]; then
     args+=('--id')
-    args+=($CFG_ID)
+    args+=("$CFG_ID")
 fi
 
 if [ -n "$CFG_PORT" ]; then
     args+=('--port')
-    args+=($CFG_PORT)
+    args+=("$CFG_PORT")
 fi
 
 if [ -n "$CFG_DEV" ]; then
-    IFS=', ' read -r -a devices <<< "${CFG_DEV}"
+    IFS="${SEPARATOR}" read -r -a devices <<< "${CFG_DEV}"
     for element in "${devices[@]}"
     do
         args+=('--dev')
-        args+=($element)
+        args+=("$element")
     done
 fi
 
 if [ -n "$CFG_MULTICAST" ]; then
     args+=('--multicast')
-    args+=($CFG_MULTICAST)
+    args+=("$CFG_MULTICAST")
 fi
 
 if [ -n "$CFG_SOURCE_IP" ]; then
     args+=('-s')
-    args+=($CFG_SOURCE_IP)
+    args+=("$CFG_SOURCE_IP")
 fi
 
 if [ -n "$CFG_TARGET_IP" ]; then
     args+=('-t')
-    args+=($CFG_TARGET_IP)
+    args+=("$CFG_TARGET_IP")
 fi
 
 /app/udp-broadcast-relay-redux ${args[@]}
